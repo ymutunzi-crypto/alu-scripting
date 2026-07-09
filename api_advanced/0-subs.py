49c5cd3dd07c6ce94916964620bb2a38603bf38c
ymutunzi-crypto <y.mutunzi@alustudent.com>
2026-07-09 18:03:35 +0000

    Task 0: How many subs?

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to query the Reddit API for subscriber count."""
+import requests
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "ALU-Student-App/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        return response.json().get("data", {}).get("subscribers", 0)
+    return 0
